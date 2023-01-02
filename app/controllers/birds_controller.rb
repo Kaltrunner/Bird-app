8f4a3a4f68f735b2dae48a3e126e91459ca354e6
Erik Russell <erikrussells@gmail.com>
2023-01-02 14:37:44 -0500

    added def show and an error message

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,8 +1,20 @@
 class BirdsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :bird_not_found
 
 def index
     birds = Bird.all
     render json: birds
+end
+
+def show
+    bird = Bird.find(params[:id])
+    render json: bird
+end
+
+private
+
+def bird_not_found
+    render json: {error: 'can not find your fucking bird!'}, status: 404
 end
 
 end
